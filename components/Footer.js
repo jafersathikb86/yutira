87985f9768059f7b31e9e6ccdcae42bb786911ea
jafersathikb86 <jafersathikb86@gmail.com>
2026-02-20 14:36:57 +0530

    Update Footer.js
    Footer updated

--- components/Footer.js
+++ components/Footer.js
@@ -33,13 +33,14 @@ export default function Footer() {
           <div>Rahul G V</div>
           <div>93448 13153</div><br/>
           <div>Vinoth Kumar S</div>
-          <div>80728 99952</div>          
-        </div>
-      </div>
-
-      <div className="py-4 text-center text-xs text-white/50 border-t border-white/10">
-        © {new Date().getFullYear()} Yutira 2026
-      </div>
+          <div>80728 99952</div>
+<div className="font-semibold text-white mb-2">Developer</div>
+<div>Jafer Sathik B</div>
+</div>
+</div>
+<div className="py-4 text-center text-xs text-white/50 border-t border-white/10">
+        © {new Date().getFullYear()} Yutira
+</div>
     </footer>
   );
 }
